bb322aef9f740cc94b81ff04cf809cca689b5de6
cihadkurt <cihadkurt@gmail.com>
2021-07-05 22:40:37 +0000

    Yeni güncellem

--- cihad.py
+++ cihad.py
@@ -1,7 +1,8 @@
 # Fill in this file with the code from parsing JSON exercise
 import json
-dosya=open("myfile.json","r")
+dosya=open("cihad.json","r")
 json_dosya=json.load(dosya)
 print("API KEY:",json_dosya["ad"])
 print("API KEY:",json_dosya["soyad"])
 print("API KEY:",json_dosya["access_token"])
+print("Yeni eklenen satır")
